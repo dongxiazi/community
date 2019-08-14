966972683900cfa6e786d0c9cb8f8639e75b3571
dongxiazi <1033643196@qq.com>
2019-08-14 21:24:07 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -6,8 +6,24 @@
 [es](https://elasticsearch.cn/explore)
 [BootStrap](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
 [GitHub OAuth](https://developer.github.com/apps/building-github-apps/creating-a-github-app/)
-
+[Spring]
 ##工具
 [Git](https://git-scm.com/download/win)
 [visual paradigm](https://www.visual-paradigm.com/cn/)
 
+##脚本
+```sql
+CREATE TABLE `user` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`account_id` VARCHAR(100) NULL,
+	`name` VARCHAR(50) NOT NULL,
+	`token` CHAR(36) NOT NULL,
+	`gmt_create` BIGINT(20) NOT NULL,
+	`gmt_modified` BIGINT(20) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+```
--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,12 +2,18 @@ package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -19,9 +25,12 @@ public class AuthorizeController {
     private String redirectUri;
     @Autowired
     private GithubProvider githubProvider;
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/callback")
     public  String callback(@RequestParam(name = "code") String code,
-                            @RequestParam(name = "state") String state){
+                            @RequestParam(name = "state") String state,
+                            HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -29,8 +38,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
     String token=    githubProvider.getAccessToken(accessTokenDTO);
-    GithubUser user=githubProvider.getUser(token);
-        System.out.println(user.getName());
-        return "index";
+    GithubUser githubUser=githubProvider.getUser(token);
+       if (githubUser!=null){
+           User user = new User();
+           user.setName(githubUser.getName());
+           user.setToken(UUID.randomUUID().toString());
+           user.setAccountId(githubUser.getId()+"");
+           user.setGmtCreate(System.currentTimeMillis());
+           user.setGmtModified(user.getGmtCreate());
+           userMapper.insert(user);
+        //登录成功 session在HttpServletRequest 写好session 即在银行里建了账号
+           request.getSession().setAttribute("user",githubUser);
+           return "redirect:/";
+       }else {
+         //重新登录
+           return "redirect:/";
+       }
     }
 }
